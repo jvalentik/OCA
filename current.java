ade8c12279f4456988661388c5f098c5fdfd2c54
powerj-sk <johny.valentik@gmail.com>
2015-10-10 13:13:09 +0200

    varargs 2

--- current.java
+++ current.java
@@ -1,9 +1,13 @@
 //testing varargs
 public class current {
-	public static void main(String...params) {
-		System.out.println(params.length);
-		for (String parameter : params) {
-			System.out.println(parameter);
-		}
+	public static void main(String[] params) {
+		numbers(1, 2, 3);		
+	}
+	
+	private static void numbers(int...nums) {
+		System.out.println(nums.length);
+		for (int number : nums) {
+			System.out.println(number);
+		}		
 	}
 }
